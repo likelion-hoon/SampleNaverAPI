d930bc40820fcd99142134fad0c983a63b79410f
likelion-hoon <monki0120@naver.com>
2016-08-07 11:49:26 +0000

    현재위치 표시 성공

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,29 +3,30 @@ require 'nokogiri'
 
 class HomeController < ApplicationController
   def index
-    
+
   end
-  
+
   def search
     query = params[:query]
     queryUrl = "https://openapi.naver.com/v1/search/local.xml?query="+query
-    
+
     uri = URI(URI.encode(queryUrl))
-    
+
     req = Net::HTTP::Get.new(uri)
-    req['X-Naver-Client-Id'] = "h40urbpdCMGXC07cN0rf"
-    req['X-Naver-Client-Secret'] = "UafuNNDgZ6"
-    
+    req['X-Naver-Client-Id'] = "mqDPQhoKS1QzNJW2JDWo"
+    req['X-Naver-Client-Secret'] = "Kli7WUwHHI"
+
     res = Net::HTTP.start(uri.hostname, uri.port, :use_ssl => uri.scheme == 'https' ) {|http|
       http.request(req)
     }
-    
+
     # @ddd = res.body
-    
+
     xml_doc = Nokogiri::XML(res.body)
     @links = xml_doc.xpath("//title")
-  
-  
-  end 
-  
+
+
+
+  end
+
 end
